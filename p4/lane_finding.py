1643d87dcff59efc7a23e76ca376c1a3b6980c43
Olivier <nguyenolive@gmail.com>
2017-01-31 18:26:28 -0500

    implementing line smoothing

--- p4/lane_finding.py
+++ p4/lane_finding.py
@@ -1,15 +1,16 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import deque
 
 
-# Define a class to receive the characteristics of each line detection
+# Class to receive the characteristics of each line detection
 class Line():
     def __init__(self):
         # was the line detected in the last iteration?
         self.detected = False
         # x values of the last n fits of the line
-        self.recent_xfitted = []
+        self.recent_xfitted = deque()
         #average x values of the fitted line over the last n iterations
         self.bestx = None
         #polynomial coefficients averaged over the last n iterations
@@ -26,7 +27,165 @@ class Line():
         self.allx = None
         #y values for detected line pixels
         self.ally = None
+        # num of iterations to keep
+        self.n = 10
 
+    def smooth_fit(self):
+        self.recent_xfitted.append(self.allx)
+
+        if len(self.recent_xfitted) > self.n:
+            self.recent_xfitted.popleft()
+
+        self.best_x = np.mean(self.recent_xfitted, axis=0)
+        self.best_fit = np.polyfit(self.bestx, self.ally, 2)
+
+
+    def compute_radius_curvature(self):
+        y_eval = np.max(self.ally) / 2
+
+        # Define conversions in x and y from pixels space to meters
+        ym_per_pix = 30/720 # meters per pixel in y dimension
+        xm_per_pix = 3.7/700 # meters per pixel in x dimension
+
+        # Fit new polynomials to x,y in world space
+        fit_cr = np.polyfit(self.ally * ym_per_pix, self.allx * xm_per_pix, 2)
+        # Calculate the new radii of curvature
+        curverad = ((1 + (2 * fit_cr[0] * y_eval * ym_per_pix + fit_cr[1])**2)**1.5) / np.absolute(2*fit_cr[0])
+        # Now our radius of curvature is in meters
+        return curverad
+
+
+class LaneDetector():
+    def __init__(self):
+        self.left_line = Line()
+        self.right_line = Line()
+        self.center = 0
+    
+    def pipeline():
+        pass
+
+
+def detect_lines(img, left_line, right_line):
+    # Assume you now have a new warped binary image
+    # from the next frame of video (also called "binary_warped")
+    # It's now much easier to find line pixels!
+    nonzero = img.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    margin = 100
+    left_lane_inds = ((nonzerox > (left_line.current_fit[0]*(nonzeroy**2) 
+        + left_line.current_fit[1]*nonzeroy + left_line.current_fit[2] - margin)) & (nonzerox < (left_line.current_fit[0]*(nonzeroy**2) 
+        + left_line.current_fit[1]*nonzeroy + left_line.current_fit[2] + margin)))
+
+    right_lane_inds = ((nonzerox > (right_line.current_fit[0]*(nonzeroy**2) 
+        + right_line.current_fit[1]*nonzeroy + right_line.current_fit[2] - margin)) & (nonzerox < (right_line.current_fit[0]*(nonzeroy**2) 
+        + right_line.current_fit[1]*nonzeroy + right_line.current_fit[2] + margin)))
+
+    # Again, extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    # update Line objects
+    left_line.allx = leftx
+    left_line.ally = lefty
+    left_line.current_fit = left_fit 
+
+    right_line.allx = rightx
+    right_line.ally = righty
+    right_line.current_fit = right_fit
+
+    return left_line, right_line
+
+
+def detect_lines_blind(binary_warped):
+    left_line = Line()
+    right_line = Line()
+
+    histogram = np.sum(binary_warped[int(binary_warped.shape[0]/2):,:], axis=0)
+    #plt.plot(histogram)
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+
+    midpoint = np.int(histogram.shape[0]/2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0]/nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window+1)*window_height
+        win_y_high = binary_warped.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2)
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2)
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    # Return Line objects
+    left_line.allx = leftx
+    left_line.ally = lefty
+    left_line.current_fit = left_fit
+
+    right_line.allx = rightx
+    right_line.ally = righty
+    right_line.current_fit = right_fit
+
+    if leftx.any() or rightx.any():
+        left_line.detected = True
+        right_line.detected = True
+
+    return left_line, right_line
 
 def lane_fit(binary_warped):
     histogram = np.sum(binary_warped[int(binary_warped.shape[0]/2):,:], axis=0)
--- p4/perspective_transform.py
+++ p4/perspective_transform.py
@@ -20,3 +20,23 @@ def warp(img):
 
     warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)
     return warped, src, dst
+
+def compute_perspective_matrix(img):
+    img_size = img.shape[1], img.shape[0] # if color image, then use img.shape[1::-1]
+
+    src = np.float32(
+        [[(img_size[0] / 2) - 60, img_size[1] / 2 + 100],
+        [((img_size[0] / 6) - 80), img_size[1]],
+        [(img_size[0] * 5 / 6) + 105, img_size[1]],
+        [(img_size[0] / 2 + 105), img_size[1] / 2 + 100]])
+
+    dst = np.float32(
+        [[(img_size[0] / 4), 0],
+        [(img_size[0] / 4), img_size[1]],
+        [(img_size[0] * 3 / 4), img_size[1]],
+        [(img_size[0] * 3 / 4), 0]])
+
+    M = cv2.getPerspectiveTransform(src, dst)
+    Minv = cv2.getPerspectiveTransform(dst, src)
+
+    return M, Minv
